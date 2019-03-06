4ec1018cdfe2b16bd1a42dcba7aa997c0d91b26e
Max Cramer <maximiliancramer@yahoo.com>
2019-03-06 12:22:59 +0000

    added all routes to front end app.js

--- src/app.js
+++ src/app.js
@@ -13,8 +13,12 @@ import SecureRoute from './components/common/SecureRoute';
 import Header from './components/Header';
 import AuthLogin from './components/auth/Login';
 import AuthRegister from './components/auth/Register';
-
-
+import UsersIndex from './components/users/Index';
+import UsersEdit from './components/users/Edit';
+import UsersShow from './components/users/Show';
+import RoomsIndex from './components/rooms/Index';
+import RoomsEdit from './components/rooms/Edit';
+import RoomsShow from './components/rooms/Show';
 
 
 class App extends React.Component {
@@ -25,7 +29,12 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/login" component={AuthLogin} />
           <Route exact path="/register" component={AuthRegister} />
-
+          <Route path="/users/:id/edit" component={UsersEdit} />
+          <Route path="/users/:id" component={UsersShow} />
+          <Route path ="/users" component = {UsersIndex} />
+          <Route path ="/rooms/:id/edit" component = {RoomsEdit} />
+          <Route path ="/rooms/:id" component = {RoomsShow} />
+          <Route path ="/rooms" component = {RoomsIndex} />
         </Switch>
       </main>
     );
