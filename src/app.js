2aa2e794153d27316422e56cf747b92259945fb8
Max Cramer <maximiliancramer@yahoo.com>
2019-03-06 12:18:33 +0000

    started build front end, creating routes on app.js

--- src/app.js
+++ src/app.js
@@ -3,3 +3,37 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import './scss/style.scss';
+
+
+
+// COMPONENTS RENDER HERE
+
+// import ROUTES
+import SecureRoute from './components/common/SecureRoute';
+import Header from './components/Header';
+import AuthLogin from './components/auth/Login';
+import AuthRegister from './components/auth/Register';
+
+
+
+
+class App extends React.Component {
+  render() {
+    return(
+      <main>
+        <Header />
+        <Switch>
+          <Route exact path="/login" component={AuthLogin} />
+          <Route exact path="/register" component={AuthRegister} />
+
+        </Switch>
+      </main>
+    );
+  }
+}
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
